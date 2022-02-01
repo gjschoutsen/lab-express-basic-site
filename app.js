a065216f7b9d1db0196df6f7a1be6517f15a53fb
Gertjan Schoutsen <gjs@Gertjans-MacBook-Pro.local>
2022-02-01 16:14:02 +0100

    started the lab 2

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (request,response, next) => response.sendFile(__firname = `/views/home-page.html`));
-app.get('/about', (request,response, next) => response.sendFile(__firname = `/views/about-page.html`));
-app.get('/works', (request,response, next) => response.sendFile(__firname = '/views/works-page.html'));
-app.get('/photogallery', (request,response, next) => response.sendFile(__firname = '/views/photo-gallery-page.html'));
+app.get('/', (request,response, next) => response.sendFile(__dirname + `/views/home-page.html`));
+app.get('/about', (request,response, next) => response.sendFile(__dirname + `/views/about-page.html`));
+app.get('/works', (request,response, next) => response.sendFile(__dirname + `/views/works-page.html`));
+app.get('/photogallery', (request,response, next) => response.sendFile(__dirname + `/views/photo-gallery-page.html`));
 
 app.listen(3001);
